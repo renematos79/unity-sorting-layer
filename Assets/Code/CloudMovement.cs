9d0e33b0e1016054b963d1ed387248d06bb82bc6
renato matos <renato.matos79@gmail.com>
2017-08-14 23:34:52 -0300

    first commit

---
+++ Assets/Code/CloudMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+	public float Speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Translate (new Vector2 (this.Speed * Time.deltaTime, 0));
+	}
+}
---
+++ Assets/Code/DayTransition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTransition : MonoBehaviour {
+
+	public float AlphaDecrement = 0.001f;
+	private SpriteRenderer _SpriteRenderer;
+	private Color _OldColor;
+	private float _Alpha = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		_SpriteRenderer = GetComponent<SpriteRenderer> ();
+		_OldColor = _SpriteRenderer.color;
+		_Alpha = _SpriteRenderer.color.a;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_Alpha = _Alpha - AlphaDecrement;
+		if (_Alpha <= 0) {
+			_Alpha = _OldColor.a;
+		}
+		_SpriteRenderer.color = new Color (_OldColor.r, _OldColor.g, _OldColor.b, _Alpha);
+	}
+}
